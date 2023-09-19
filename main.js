a978fa5dfb3d1f71fd3238535d6811706dc4f6f3
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2023-09-19 11:09:35 +0200

    adding some colors

---
+++ main.js
@@ -0,0 +1,58 @@
+'use strict';
+//Variables
+const select = document.querySelector('.js-select');
+const stone = document.querySelector('.js-stone');
+const paper = document.querySelector('.js-paper');
+const scissors = document.querySelector('.js-scissors');
+const btn = document.querySelector('.js-btn');
+const msg = document.querySelector('.js-msg');
+const player = document.querySelector('.js-player');
+const computer = document.querySelector('.js-computer');
+
+//Funciones///////////////
+
+//numero aleatorio asignado a valor papel, tijera o piedra
+function compare() {
+  getluck();
+  const computerChoice = getluck();
+  const playerChoice = select.value;
+  
+  if (playerChoice===computerChoice ){ msg.innerHTML='Empate'}
+  else if (
+    (playerChoice === 'piedra' && computerChoice === 'tijeras') ||
+    (playerChoice === 'papel' && computerChoice === 'piedra') ||
+    (playerChoice === 'tijeras' && computerChoice === 'papel')
+  ) {
+    msg.innerHTML = 'Ganaste';
+  } else {
+    msg.innerHTML = 'Perdiste';
+  }
+  console.log(computerChoice);
+  console.log(playerChoice)
+
+}
+function getluck() {
+  getRandomNumber(9);
+  const randomNum = getRandomNumber(9);
+  if (randomNum <= 3) {
+    return 'piedra';
+  } else if (randomNum <= 7) {
+    return 'papel';
+  } else {
+    return 'tijeras';
+  }
+}
+console.log(getluck());
+
+function getRandomNumber( max) {
+    return Math.ceil(Math.random()*max);}
+
+console.log(getRandomNumber(9));
+
+function handleClickPlay(event) {
+  event.preventDefault();
+ compare();
+}
+//Eventos
+btn.addEventListener('click', handleClickPlay);
+
